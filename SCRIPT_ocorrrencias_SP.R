249634b3d0e11a632f83fd519d6f013bb33591ca
Bruno Borges <brunoborges2004@yahoo.com.br>
2022-10-25 00:57:09 -0300

    Melhora na criacao do dataframa pivot_longer, criacao de colunas com totais por tipo_ocorrencia e graficos por tipo_ocorrencia, total_crimes e por regiao

--- SCRIPT_ocorrrencias_SP.R
+++ SCRIPT_ocorrrencias_SP.R
@@ -25,90 +25,146 @@ df %>%
   mutate_all(replace_na,0) %>% 
   rowSums(na.rm = TRUE)
 
-## DATAFRAME MODELO 1
-## cria coluna com total de crimes
-
+## cria coluna total_homicidio (comecando pelo dataframe = df)
 df_temp <- df %>% 
-  # adiciona um "id" com os nomes das linhas
   rownames_to_column('id') %>% 
-  # empilha as colunas A até F. ao invés de -id poderia ser A:F
-  gather(key, val, 6:28) %>% 
-  # convert val em numeric
-  mutate(val = as.numeric(val)) %>% 
-  # agrupa pelo id
-  group_by(id) %>%
-  # cria soma total e proporcao (desconsiderando NAs)
-  mutate(total_crimes = sum(val, na.rm = TRUE)) %>% 
-  # desagrupa
+  gather(key, val, contains("homicidio")) %>% 
+  group_by(id) %>% 
+  mutate(total_homicidio = sum(val, na.rm = TRUE)) %>% 
   ungroup() %>% 
-  # joga A:F nas colunas novamente
   spread(key, val) %>% 
-  # reordena colunas e retira o "id"
-  select(ano,mes,id_municipio, regiao_ssp,total_crimes,everything(),-id)
-df_temp %>% View()
+  select(everything(),-id)
 
-## criando coluna datatime
+## cria coluna total_lesao (identando pelo dataframe = df_temp)
 df_temp <- df_temp %>% 
-  mutate(dia = 1) %>% 
-  mutate(data = lubridate::make_date(year = ano,month = mes,day = dia))
-df_temp$data[1] %>% class
+  rownames_to_column('id') %>% 
+  gather(key, val, contains("lesao")) %>% 
+  group_by(id) %>% 
+  mutate(total_lesao = sum(val, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  spread(key, val)%>% 
+  select(everything(),-id)
 
-## teste de agrupamento
-df_temp %>%
-  group_by(data) %>%
-  summarise(total_crimes = sum(total_crimes)) %>% 
-  filter(data == "2005-01-01")
+## cria coluna latrocinio (identando pelo dataframe = df_temp)
+df_temp <- df_temp %>% 
+  rownames_to_column('id') %>% 
+  gather(key, val, contains("latrocinio")) %>% 
+  group_by(id) %>% 
+  mutate(total_latrocinio = sum(val, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  spread(key, val)%>% 
+  select(everything(),-id)
 
-## DATAFRAME MODELO 2
+## cria coluna estupro (identando pelo dataframe = df_temp)
+df_temp <- df_temp %>% 
+  rownames_to_column('id') %>% 
+  gather(key, val, contains("estupro")) %>% 
+  group_by(id) %>% 
+  mutate(total_estupro = sum(val, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  spread(key, val)%>% 
+  select(everything(),-id)
+    
+## cria coluna roubo (identando pelo dataframe = df_temp)
+df_temp <- df_temp %>% 
+  rownames_to_column('id') %>% 
+  gather(key, val, contains("roubo")) %>% 
+  group_by(id) %>% 
+  mutate(total_roubo = sum(val, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  spread(key, val)%>% 
+  select(everything(),-id)
+
+## cria coluna furto (identando pelo dataframe = df_temp)
+df_temp <- df_temp %>% 
+  rownames_to_column('id') %>% 
+  gather(key, val, contains("furto")) %>% 
+  group_by(id) %>% 
+  mutate(total_furto = sum(val, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  spread(key, val)%>% 
+  select(everything(),-id)
+
+## seleciona somente colunas de interessa
+df_temp <- df_temp %>% 
+              select(ano,
+                     mes,
+                     id_municipio,
+                     regiao_ssp,
+                     starts_with("total_"),
+                     -total_de_estupro,
+                     -total_de_roubo_outros)
+
 ## aplica pivotagem na base de dados partindo dos crimes
-df_temp2 <- df %>% 
-              tidyr::pivot_longer(
-                cols = (tidyselect::contains("homicidio")|
-                        tidyselect::contains("lesao")|
-                        tidyselect::contains("latrocinio")|
-                        tidyselect::contains("estupro")|
-                        tidyselect::contains("roubo")|
-                        tidyselect::contains("furto")),
-                names_to = "tipo_ocorrencia",
-                values_to = "crimes")
-df_temp2 %>% View
+df_temp <- df_temp %>% 
+            tidyr::pivot_longer(
+              cols = tidyselect::starts_with("total_"),
+              names_to = "tipo_ocorrencia",
+              values_to = "crimes")
+df_temp$tipo_ocorrencia <- stringr::str_replace(df_temp$tipo_ocorrencia,pattern = "total_",replacement = "")
+df_temp %>% View
 
-## criando coluna datatime (DATAFRAME MODELO 2)
+------------    
+    
+## criando coluna datatime ("somente se necessario")
 df_temp2 <- df_temp2 %>% 
   mutate(dia = 1) %>% 
   mutate(data = lubridate::make_date(year = ano,month = mes,day = dia))
 df_temp2$data[1] %>% class
 
-#confere
-df_temp %>% group_by(data) %>% filter(data == "2005-01-01") %>% summarise(n=sum(total_crimes))
-df_temp2 %>% filter(ano == 2005 & mes==1) %>% summarise(n=sum(crimes,na.rm = T))
 
 # Visualização ------------------------------------------------------------
 
 
 ## curva total de crimes por dia (labels agrupados por ano)
 df_temp %>% 
-  group_by(data) %>% 
-  summarise(total_crimes = sum(total_crimes)) %>% 
-  ggplot2::ggplot(aes(x=data, y=total_crimes,group=1))+
+  group_by(ano) %>%
+  summarise(crimes = sum(crimes,na.rm = T)) %>%
+  ggplot2::ggplot(aes(x=ano, y=crimes))+
   ggplot2::geom_line()+
-  ggplot2::scale_x_date(date_breaks = "1 year", date_labels = "%Y")+
+  ggplot2::geom_point()+
+  ggplot2::scale_x_continuous(n.breaks = 18)+
+  ggplot2::scale_y_continuous(limits =  c(0,1800000),
+                              labels = scales::number_format(accuracy = 1,
+                                                             big.mark = "."))+
   ggplot2::theme_classic()
 
+
 ## curva de crimes por tipo_ocorrencia
-df_temp2 %>% 
-  group_by(data,tipo_ocorrencia) %>%
+df_temp %>% 
+  group_by(ano,tipo_ocorrencia) %>%
   summarise(crimes = sum(crimes,na.rm = T)) %>%
-  ggplot2::ggplot(aes(x=data, y=crimes,color=tipo_ocorrencia))+
-  ggplot2::geom_line(show.legend = F)+
-  ggplot2::scale_x_date(date_breaks = "1 year", date_labels = "%Y")+
+  ggplot2::ggplot(aes(x=ano, y=crimes,color=tipo_ocorrencia))+
+  ggplot2::geom_line()+
+  ggplot2::geom_point()+
+  ggplot2::scale_x_continuous(n.breaks = 18)+
+  ggplot2::scale_y_continuous(expand = expansion(add = c(0,100000)),
+                     labels = scales::number_format(accuracy = 1,
+                                                    big.mark = "."))+
   ggplot2::theme_classic()
   
 
-## total de crimes por ano
+## total de crimes por ano e por tipo_ocorrencia
 df_temp %>% 
-  group_by(ano) %>% 
-  summarise(total_crimes = sum(total_crimes)) %>% 
-  ggplot2::ggplot(aes(x=ano, y=total_crimes))+
+  group_by(ano,tipo_ocorrencia) %>% 
+  summarise(crimes = sum(crimes)) %>% 
+  ggplot2::ggplot(aes(x=ano, y=crimes,fill=tipo_ocorrencia))+
   ggplot2::geom_col()+
+  ggplot2::theme_classic()+
+  ggplot2::scale_x_continuous(n.breaks = 18)+
+  ggplot2::scale_y_continuous(expand = expansion(add = c(0,100000)),
+                              labels = scales::number_format(accuracy = 1,
+                                                             big.mark = "."))+
   ggplot2::theme_classic()
+
+
+## total de crimes por ano e por tipo_ocorrencia
+df_temp %>% 
+  group_by(ano,regiao_ssp) %>% 
+  summarise(crimes = sum(crimes)) %>% 
+  ggplot2::ggplot(aes(x=ano, y=crimes,fill=regiao_ssp))+
+  ggplot2::geom_col(position = "fill")+
+  ggplot2::theme_classic()+
+  ggplot2::scale_x_continuous(n.breaks = 18)+
+  ggplot2::theme_classic()
+
